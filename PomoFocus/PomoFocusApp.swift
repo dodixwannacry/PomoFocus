775b3d1e3d71727002a842512f1a223978fbac7f
dodixwannacry <132576872+dodixwannacry@users.noreply.github.com>
2023-12-05 11:54:30 +0100

    Initial Commit

---
+++ PomoFocus/PomoFocusApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomoFocusApp.swift
+//  PomoFocus
+//
+//  Created by Rodolfo Falanga on 05/12/23.
+//
+
+import SwiftUI
+
+@main
+struct PomoFocusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

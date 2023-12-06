2a45a56dadb1ac4eb8c2722d27b95000498a901f
dodixwannacry <132576872+dodixwannacry@users.noreply.github.com>
2023-12-06 11:59:00 +0100

    dsds

--- PomoFocus/ContentView.swift
+++ PomoFocus/ContentView.swift
@@ -1,21 +1,106 @@
 //
 //  ContentView.swift
-//  PomoFocus
+//  PomoFocusdef
 //
-//  Created by Rodolfo Falanga on 05/12/23.
+//  Created by Rodolfo Falanga on 06/12/23.
 //
 
 import SwiftUI
+import AVFoundation
+
+struct AppBtn: ViewModifier {
+    
+    let color: Color
+
+    func body(content: Content) -> some View {
+        content
+            .padding()
+            .frame(minWidth: 50, maxHeight: 50)
+            .font(.title)
+            .background(color)
+    }
+}
+
+extension Button {
+    func appBtn(color: Color = .red) -> some View {
+        modifier(AppBtn(color: color))
+    }
+}
+
+
 
 struct ContentView: View {
-    @EnvironmentObject var pomodoroModel: PomodoroModel
+    
+    @State var appState = AppState {
+        AudioServicesPlaySystemSound(1032)
+    }
+    
+    @State var timer: Timer? = nil
+    
+    
     var body: some View {
-            Home()
-                .environmentObject(pomodoroModel)
-                
-        }
+            ZStack {
+                LinearGradient(colors: [.black, .purple], startPoint: .center, endPoint: .bottom)
+                    .ignoresSafeArea()
+                VStack(spacing: 10) {
+                    Spacer()
+                    GeometryReader { geo in
+                        VStack(spacing: 10) {
+                            Text(appState.mode.rawValue)
+                                .foregroundColor(.white)
+                                .font(.system(size: 50))
+                            Text(appState.currentTimeDisplay)
+                                .foregroundColor(.white)
+                                .font(.system(size: 50))
+                        }
+                        .frame(width: geo.size.width, height: geo.size.height)
+                        .background(.purple)
+                        .clipShape(RoundedRectangle(cornerRadius: 30))
+                        
+                    }.frame(maxHeight: 150)
+                        Stepper("\(appState.workMinutes)  minute session", value: $appState.workMinutes, in: 1...99)
+                            .disabled(timer != nil)
+                            .padding()
+                            .background(.purple)
+                            .foregroundColor(.white)
+                            .font(.title)
+                            .cornerRadius(15.0)
+
+                    Stepper("\(appState.restMinutes) minute break ", value: $appState.restMinutes, in: 1...99)
+                            .disabled(timer != nil)
+                            .padding()
+                            .background(.purple)
+                            .foregroundColor(.white)
+                            .font(.title)
+                            .cornerRadius(15.0)
+                    HStack {
+                        Button("Start") {
+                            timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true){ _ in
+                                appState.next()
+                            }
+                        }.appBtn(color: .green)
+                        Button("Stop") {
+                            timer?.invalidate()
+                            timer = nil
+                        }.appBtn(color: .red)
+                        Button("Reset") {
+                            timer?.invalidate()
+                            timer = nil
+                            appState.reset()
+                        }.appBtn(color: .yellow)
+                    }
+                    Spacer()
+                }
+                .foregroundColor(.white)
+                .padding()
+            }
+            
+    }
 }
 
-#Preview {
-    ContentView()
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
+
---
+++ ViewModel/PomoFocus.swift
@@ -0,0 +1,18 @@
+//
+//  PomoFocusdefApp.swift
+//  PomoFocusdef
+//
+//  Created by Rodolfo Falanga on 06/12/23.
+//
+
+import SwiftUI
+
+@main
+struct PomoFocusdef: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
